b0a3d3e0e6f890a161b9819e72698faa4c7d8d48
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2023-12-23 17:01:55 +0000

    store terraform state in s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "lemnispace-terraform-state"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
 }
 
 provider "aws" {
