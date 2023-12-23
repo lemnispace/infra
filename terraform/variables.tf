3af942165481a21b601a0bc9e5b4ffcb8082bdfe
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2023-12-23 17:01:40 +0000

    remove unused variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,17 +7,6 @@ variable "stage_name" {
   description = "The name of the stage to deploy to"
 }
 
-variable "lambda_endpoint" {
-  type        = string
-  description = "The endpoint for the lambda function to be invoked by the API Gateway. Do not include a trailing slash. (e.g. '/posts' not '/posts/')"
-  default     = "lemnispace-services"
-}
-
-variable "lambda_arn" {
-  type        = string
-  description = "ARN of the Lambda function to be invoked by the API Gateway"
-}
-
 variable "route_hashes" {
   type        = list(string)
   description = "The hashes of the routes resources associated with the api" # this is used to trigger a redeployment
