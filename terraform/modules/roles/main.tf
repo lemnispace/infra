d0250eb2a680f142523c33f23f1c9ef84bd35087
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2023-12-23 03:05:21 +0000

    refactor roles modules

--- terraform/modules/roles/main.tf
+++ terraform/modules/roles/main.tf
@@ -1,22 +1,22 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
 resource "aws_iam_role" "execute_lambda_role" {
-  name = "execute_lambda_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+  name               = "execute_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.execute_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-
 }
--- terraform/modules/roles/outputs.tf
+++ terraform/modules/roles/outputs.tf
@@ -1,7 +1,7 @@
-output "arn" {
+output "execute_lambda_arn" {
   value = aws_iam_role.execute_lambda_role.arn
 }
 
-output "name" {
+output "execute_lambda_name" {
   value = aws_iam_role.execute_lambda_role.name
 }
