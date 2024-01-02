29d5848ed8562a1b636e2530eeae52179b1e2ff6
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-01-02 16:31:41 +0000

    debug

--- cmd/deploy/main.go
+++ cmd/deploy/main.go
@@ -68,12 +68,35 @@ type LastResponse struct {
 	Message interface{} `json:"message"`
 }
 
+/**
+// IsValidPayload checks if the github payload's hash fits with
+// the hash computed by GitHub sent as a header
+func IsValidPayload(secret, headerHash string, payload []byte) bool {
+	hash := HashPayload(secret, payload)
+	return hmac.Equal(
+		[]byte(hash),
+		[]byte(headerHash),
+	)
+}
+
+// HashPayload computes the hash of payload's body according to the webhook's secret token
+// see https://developer.github.com/webhooks/securing/#validating-payloads-from-github
+// returning the hash as a hexadecimal string
+func HashPayload(secret string, playloadBody []byte) string {
+	hm := hmac.New(sha1.New, []byte(secret))
+	hm.Write(playloadBody)
+	sum := hm.Sum(nil)
+	return fmt.Sprintf("%x", sum)
+**/
+
 func verifySignature(event LambdaEvent, secret string) (bool, error) {
 	signature := hmac.New(sha256.New, []byte(secret))
 	signature.Write([]byte(event.Body))
 	expectedMAC := signature.Sum(nil)
-
+	log.Print("Expected MAC: ", expectedMAC)
+	
 	receivedMAC, err := hex.DecodeString(strings.TrimPrefix(event.Headers.XHubSignature256, "sha256="))
+	log.Print("receivedMAC: ", expectedMAC)
 	if err != nil {
 		log.Println(err)
 		return false, fmt.Errorf("error decoding received signature: %s", err)
