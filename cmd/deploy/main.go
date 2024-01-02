58b68adcee27ce1ab036fa7aba92f4d59b52826b
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-01-02 01:57:38 +0000

    Refactor webhook event handling in main.go

--- cmd/deploy/main.go
+++ cmd/deploy/main.go
@@ -3,19 +3,59 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type WebhookEvent struct {
+	Zen         string `json:"zen"`
+	HookID      int    `json:"hook_id"`
+	Hook        Hook   `json:"hook"`
 }
 
-func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
-	fmt.Println("Event:", event)
-	fmt.Println("Context:", ctx)
+type Hook struct {
+	Type        string       `json:"type"`
+	ID          int          `json:"id"`
+	Name        string       `json:"name"`
+	Active      bool         `json:"active"`
+	Events      []string     `json:"events"`
+	Config      HookConfig   `json:"config"`
+	UpdatedAt   string       `json:"updated_at"`
+	CreatedAt   string       `json:"created_at"`
+	URL         string       `json:"url"`
+	TestURL     string       `json:"test_url"`
+	PingURL     string       `json:"ping_url"`
+	DeliveriesURL string     `json:"deliveries_url"`
+	LastResponse LastResponse `json:"last_response"`
+}
 
-	return "Hello World!", nil
+type HookConfig struct {
+	ContentType string `json:"content_type"`
+	InsecureSSL string `json:"insecure_ssl"`
+	Secret      string `json:"secret"`
+	URL         string `json:"url"`
+}
+
+type LastResponse struct {
+	Code    interface{} `json:"code"`
+	Status  string      `json:"status"`
+	Message interface{} `json:"message"`
+}
+
+func HandleRequest(ctx context.Context, event interface{}) (string, error) {
+	err_msg := "Error retrieving secret for webhook validation"
+	ssmsvc, err := NewSSMClient(ctx)
+	if err != nil {
+		log.Println(err)
+		return "", fmt.Errorf(err_msg)
+ }
+	_, err = ssmsvc.Param("/Any/infra/webhook-secret", true).GetValue(ctx)
+	if err != nil {
+		 log.Println(err)
+		 return "", fmt.Errorf(err_msg)
+	}
+	return "success", nil
 }
 
 func main() {
