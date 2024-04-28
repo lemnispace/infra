60847f946d1f7b25ee9b744de8c76d1fd8b115c5
Santiago Gomez <24705975+santiagoa58@users.noreply.github.com>
2024-04-28 14:09:30 -0700

    adds user product files s3 bucket (#6)

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -3,6 +3,10 @@ resource "random_pet" "lemnispace_services_bucket_name" {
   length = 4
   prefix = "lemnispace-services-bucket"
 }
+resource "random_pet" "lemnispace_user_product_files_bucket_name" {
+  length = 4
+  prefix = "lemnispace-user-product-files-bucket"
+}
 
 # create s3 bucket
 resource "aws_s3_bucket" "lemnispace_services_bucket" {
@@ -10,6 +14,13 @@ resource "aws_s3_bucket" "lemnispace_services_bucket" {
   tags = {
     Name = "lemnispace-services-bucket"
   }
+}
+resource "aws_s3_bucket" "lemnsipace_user_product_files_bucket" {
+  bucket = random_pet.lemnispace_user_product_files_bucket_name.id
+  tags = {
+    Name = "lemnispace-user-product-files-bucket"
+  }
+
 }
 
 # add bucket ownership controls
@@ -19,10 +30,21 @@ resource "aws_s3_bucket_ownership_controls" "lemnispace_services_ownership_contr
     object_ownership = "BucketOwnerPreferred"
   }
 }
+resource "aws_s3_bucket_ownership_controls" "lemnispace_user_product_files_ownership_controls" {
+  bucket = aws_s3_bucket.lemnsipace_user_product_files_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
 # add bucket acl (access control list)
 resource "aws_s3_bucket_acl" "lemnispace_services_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.lemnispace_services_ownership_controls]
   bucket     = aws_s3_bucket.lemnispace_services_bucket.id
+  acl        = "private"
+}
+resource "aws_s3_bucket_acl" "lemnispace_user_product_files_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.lemnispace_user_product_files_ownership_controls]
+  bucket     = aws_s3_bucket.lemnsipace_user_product_files_bucket.id
   acl        = "private"
 }
